fc30775db1acc38ba840b6534f43da3939905e5a
Pete Goodman <thegingerbloke@users.noreply.github.com>
2018-08-17 11:31:20 +0100

    Tweak frontend layout, push announcements to social media

--- phx/social/apps.py
+++ phx/social/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_save
-from .signals import save_news, save_results
+from .signals import save_news, save_results, save_announcements
 
 
 class SocialConfig(AppConfig):
@@ -9,10 +9,16 @@ class SocialConfig(AppConfig):
     def ready(self):
         from news.models import News
         from results.models import Result
+        from home.models import Announcement
 
         post_save.connect(save_news, sender=News, dispatch_uid="add_news")
         post_save.connect(
             save_results,
             sender=Result,
             dispatch_uid="add_results"
+        )
+        post_save.connect(
+            save_announcements,
+            sender=Announcement,
+            dispatch_uid="add_announcements"
         )
--- phx/social/signals.py
+++ phx/social/signals.py
@@ -1,5 +1,7 @@
 from django.urls import reverse
 from django.conf import settings
+from django.utils.html import strip_tags
+from django.template.defaultfilters import truncatechars
 
 
 def save_news(sender, instance, created, **kwargs):
@@ -14,9 +16,20 @@ def save_results(sender, instance, created, **kwargs):
         save('Results', instance.fixture.title, url)
 
 
-def save(model, title, url):
+def save_announcements(sender, instance, created, **kwargs):
+    if created:
+        url = get_announcements_url()
+        # shorten annoucement to tweet length if necessary
+        announcement = instance.announcement
+        announcement = strip_tags(announcement)
+        announcement = truncatechars(announcement, 120)
+        # don't re-post announcements - they may no longer be relevant
+        save('Notice', announcement, url, True)
+
+
+def save(model, title, url, reposted=False):
     from .models import Social
-    social = Social(model=model, title=title, url=url)
+    social = Social(model=model, title=title, url=url, reposted=reposted)
     social.save()
     social.post()
 
@@ -29,3 +42,7 @@ def get_news_url(obj):
 def get_results_url(obj):
     url = reverse('results-index')
     return '{0}{1}'.format(settings.HOST, url)
+
+
+def get_announcements_url():
+    return settings.HOST
