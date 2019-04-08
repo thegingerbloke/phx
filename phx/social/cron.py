d9962377a0320960406d05f7ab4006b023d75d4d
Pete G <341682+orangespaceman@users.noreply.github.com>
2019-04-08 09:09:08 +0100

    Update social cron running time

--- phx/social/cron.py
+++ phx/social/cron.py
@@ -4,8 +4,11 @@ from .models import Social
 
 
 class SocialCronPost(CronJobBase):
-    RUN_EVERY_MINS = 60
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    RUN_AT_TIMES = [
+        '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00',
+        '17:00', '18:00', '19:00', '20:00', '21:00'
+    ]
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'phx.social.cron_post'
 
     def do(self):
